8e0d22af3a619985bfcb1b7d009b3d1bc4815f4b
ryosei <ryosei_wada@respawn.jp>
2024-07-30 15:09:38 +0900

    環境構築

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
+// import "./styles/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/styles/theme.tsx
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: "#319795",
+    secondary: "#2B6CB0",
+  },
+  fonts: {
+    heading: "Georgia, serif",
+    body: "Arial, sans-serif",
+  },
+});
+
+export default theme;
